c2d32f4bd122576ad85a84948a140c2ab46e9ed9
chetmehta23 <54516633+chetmehta23@users.noreply.github.com>
2019-09-09 10:09:24 -0400

    Add files via upload

---
+++ app/public/patientdata.php
@@ -0,0 +1,23 @@
+[
+  {
+    "patientGuid": "1",
+    "firstName": "John",
+    "lastName": "Dennis",
+    "dob": "1990-12-03",
+    "sexAtBirth": "M"
+  },
+  {
+    "patientGuid": "2",
+    "firstName": "Matt",
+    "lastName": "Murphy",
+    "dob": "1985-11-04",
+    "sexAtBirth": "M"
+  },
+  {
+    "patientGuid": "3",
+    "firstName": "Bob",
+    "lastName": "Jonas",
+    "dob": "1995-06-09",
+    "sexAtBirth": "M"
+  }
+]
